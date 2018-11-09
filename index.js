4257f24eb07b698fe6d412577c5eae2614c0eb44
pkprosol <peter.prosol@gmail.com>
2018-11-09 12:23:39 -0500

    do not allow email with spaces

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const validZipcodes = new Set(require('./data/validZipcodes'))
 
+// TODO add Regex for email
 module.exports = {
-  isValidEmail: email => email.length > 3 && email.includes('@'),
+  isValidEmail: email => email.length > 3 && email.includes('@') && !email.includes(' '), 
   isValidPassword: password => !!( password && password.length >= 6 ),
   isValidPhone: phoneNumber => phoneNumber.length > 6,
   isValidZipcode: zipcode => validZipcodes.has(String(zipcode))
