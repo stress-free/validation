7a65a7a1504ab85a0b94e372274c36e7d36700cd
Paul Hoang <paulkhoahoang@gmail.com>
2018-11-19 14:09:29 -0800

    Add validation functions for first/last names

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ module.exports = {
   isValidEmail: email => email.length > 3 && email.includes('@') && !email.includes(' '), 
   isValidPassword: password => !!( password && password.length >= 6 ),
   isValidPhone: phoneNumber => phoneNumber.length > 6,
-  isValidZipcode: zipcode => validZipcodes.has(String(zipcode))
+  isValidZipcode: zipcode => validZipcodes.has(String(zipcode)),
+  isValidFirstName: firstName => !!firstName && firstName.length > 0,
+  isValidLastName: lastName => !!lastName && lastName.length > 0,
 }
