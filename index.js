fc493c01a6cc348f8a57535b929f8db2793871bf
Paul Hoang <paulkhoahoang@gmail.com>
2018-11-19 14:45:43 -0800

    Add validation functions for first/last names (#5)
    * Add validation functions for first/last names
    
    * Add test files
    
    * Add automatic tagging
    
    * Use single function for name validation
    
    * Remove duplicate test case
    
    * Fix circleci config

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ module.exports = {
   isValidEmail: email => email.length > 3 && email.includes('@') && !email.includes(' '), 
   isValidPassword: password => !!( password && password.length >= 6 ),
   isValidPhone: phoneNumber => phoneNumber.length > 6,
-  isValidZipcode: zipcode => validZipcodes.has(String(zipcode))
+  isValidZipcode: zipcode => validZipcodes.has(String(zipcode)),
+  isValidName: name => !!name && name.length > 0,
 }
