6bafce93378c2ff5260ea7aab7bb452b6c1072ad
Paul Hoang <paulkhoahoang@gmail.com>
2019-01-03 11:29:51 -0800

    Improve isValidPhone() with google-libphonenumber
    Previously, we only checked the length of the phone to see if it's valid.
    
    Now, we actually use a library to validate it for us. Add test cases
    as well.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance()
 const validZipcodes = new Set(require('./data/validZipcodes'))
 
 // TODO add Regex for email
 module.exports = {
   isValidEmail: email => email.length > 3 && email.includes('@') && !email.includes(' '), 
   isValidPassword: password => !!( password && password.length >= 6 ),
-  isValidPhone: phoneNumber => phoneNumber.length > 6,
+  isValidPhone: (phone, region = 'US') => {
+    // Reject numbers with letters in them because google-libphonenumber accepts numbers with letters sometimes
+    // See: https://github.com/googlei18n/libphonenumber/blob/master/FAQ.md#why-does-the-library-treat-some-non-digit-characters-as-digits
+    if (/[a-zA-Z]/.test(phone)) {
+      return false
+    }
+
+    let number
+    try {
+      number = phoneUtil.parse(phone, region)
+    } catch (e) {
+      return false
+    }
+
+    return phoneUtil.isValidNumberForRegion(number, region)
+  },
   isValidZipcode: zipcode => validZipcodes.has(String(zipcode)),
   isValidName: name => !!name && name.length > 0,
 }
